5781f52e472d2fb15f0eff426f7f1390554a0976
Anton Chertash <anton.chertash@gmail.com>
2013-06-05 17:14:46 +0300

    Refactor "date"-fields for text representation in DB. Date will be stored by inner function (date, time etc.)

--- ToDoManager/src/com/teamdev/beans/Task.java
+++ ToDoManager/src/com/teamdev/beans/Task.java
@@ -4,41 +4,58 @@ import java.util.Calendar;
 
 public class Task {
 	
+	private int id;
 	private String title;
 	private String description;
-	private Calendar creationDate;
-	private Calendar lastChangedDate;
+	private String creationDate;
+	private String lastChangedDate;
 	private String state;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	} 
 	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getDescription() {
 		return description;
 	}
+	
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public Calendar getCreationDate() {
+	
+	public String getCreationDate() {
 		return creationDate;
 	}
-	public void setCreationDate(Calendar creationDate) {
+	
+	public void setCreationDate(String creationDate) {
 		this.creationDate = creationDate;
 	}
-	public Calendar getLastChangedDate() {
+	
+	public String getLastChangedDate() {
 		return lastChangedDate;
 	}
-	public void setLastChangedDate(Calendar lastChangedDate) {
+	
+	public void setLastChangedDate(String lastChangedDate) {
 		this.lastChangedDate = lastChangedDate;
 	}
+	
 	public String getState() {
 		return state;
 	}
+	
 	public void setState(String state) {
 		this.state = state;
 	}
-	
 }
